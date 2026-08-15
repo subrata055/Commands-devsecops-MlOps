e3926f2c2b25a91096bbd5961d9d269df6f4773c
Subrata Sikdar <103638344+subrata055@users.noreply.github.com>
2026-08-15 20:40:45 +0530

    Update output.tf

--- Devops/scripts/Terraform/output.tf
+++ Devops/scripts/Terraform/output.tf
@@ -1,15 +1,15 @@
 output "cluster_id" {
-  value = aws_eks_cluster.devopsshack.id
+  value = aws_eks_cluster.Mega-Project.id
 }
 
 output "node_group_id" {
-  value = aws_eks_node_group.devopsshack.id
+  value = aws_eks_node_group.Mega-Project.id
 }
 
 output "vpc_id" {
-  value = aws_vpc.devopsshack_vpc.id
+  value = aws_vpc.Mega-Project_vpc.id
 }
 
 output "subnet_ids" {
-  value = aws_subnet.devopsshack_subnet[*].id
+  value = aws_subnet.Mega-Project_subnet[*].id
 }
